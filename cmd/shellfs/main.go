bce25fab55e9aa49734b5eae78a356fe4cb7b8f6
Toyam Cox <Vaelatern@voidlinux.org>
2021-05-26 23:05:17 -0400

    Problem: Can't execute commands as files
    Solution: Add a basic static file.

--- cmd/shellfs/main.go
+++ cmd/shellfs/main.go
@@ -4,8 +4,13 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"os"
+	"syscall"
+
+	"os/exec"
+	"path/filepath"
 
 	"bazil.org/fuse"
 	"bazil.org/fuse/fs"
@@ -58,13 +63,15 @@ type Dir struct {
 	origin string
 }
 
-// File is a file in the filesystem. It has a source location
-type File struct {
+// CommandFile is a file in the filesystem. It has a source location
+type CommandFile struct {
 	from string
 }
 
 type CommandFileHandle struct {
-	from string
+	from   string
+	stdout io.ReadCloser
+	cmd    exec.Cmd
 }
 
 //////////////////////////////////////////////////////////////////
@@ -72,7 +79,6 @@ type CommandFileHandle struct {
 /////////////////////////////////////////////////////////////////
 
 func (sfs ShellFileSystem) Root() (fs.Node, error) {
-	fmt.Printf("Returning Root()\n")
 	return Dir{origin: sfs.origin}, nil
 }
 
@@ -81,38 +87,42 @@ func (sfs ShellFileSystem) Root() (fs.Node, error) {
 /////////////////////////////////////////////////////////////////
 
 func (d Dir) Lookup(ctx context.Context, name string) (fs.Node, error) {
-	fmt.Printf("%s\n", name)
-	return File{from: d.origin + "my-info"}, nil
+	return CommandFile{from: filepath.Join(d.origin, "my-info")}, nil
 }
 
 func (d Dir) ReadDirAll(ctx context.Context) ([]fuse.Dirent, error) {
-	fmt.Println("ReadDirAll")
 	return []fuse.Dirent{
 		{Inode: 2, Name: "my-info", Type: fuse.DT_File},
 	}, nil
 }
 
 func (d Dir) Attr(ctx context.Context, attr *fuse.Attr) error {
-	fmt.Printf("%#v\n", attr)
 	attr.Inode = 1
 	attr.Mode = os.ModeDir | 0o755
 	return nil
 }
 
 //////////////////////////////////////////////////////////////////
-// File
+// CommandFile
 /////////////////////////////////////////////////////////////////
 
-func (f File) Open(ctx context.Context, req *fuse.OpenRequest, resp *fuse.OpenResponse) (fs.Handle, error) {
-	fmt.Printf("%#v\n", req)
-	fmt.Printf("%#v\n", resp)
-	return CommandFileHandle{from: f.from}, nil
+func (f CommandFile) Open(ctx context.Context, req *fuse.OpenRequest, resp *fuse.OpenResponse) (fs.Handle, error) {
+	cmd := exec.Cmd{Path: "./cmd", Dir: f.from}
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := cmd.Start(); err != nil {
+		log.Print(err)
+		return nil, err
+	}
+	resp.Flags |= fuse.OpenNonSeekable
+	return CommandFileHandle{from: f.from, cmd: cmd, stdout: stdout}, nil
 }
 
-func (f File) Attr(ctx context.Context, attr *fuse.Attr) error {
-	fmt.Printf("%#v\n", attr)
+func (f CommandFile) Attr(ctx context.Context, attr *fuse.Attr) error {
 	// EOF will be signaled when Read returns less than asked, so...
-	attr.Size = 3 // This must be as large or larger than the target data
+	attr.Size = 10000 // This must be as large or larger than the target data
 	attr.Mode = 0o644
 	return nil
 }
@@ -122,14 +132,17 @@ func (f File) Attr(ctx context.Context, attr *fuse.Attr) error {
 /////////////////////////////////////////////////////////////////
 
 func (cfh CommandFileHandle) Read(ctx context.Context, req *fuse.ReadRequest, resp *fuse.ReadResponse) error {
-	fmt.Printf("%#v\n", req)
-	fmt.Printf("%#v\n", resp)
-	resp.Data = []byte("Hi\n")
-	return nil
+	buf := make([]byte, req.Size)
+	n, err := io.ReadFull(cfh.stdout, buf)
+	if err == io.ErrUnexpectedEOF || err == io.EOF {
+		err = nil
+	}
+	resp.Data = buf[:n]
+	return err
 }
 
 func (cfh CommandFileHandle) Release(ctx context.Context, req *fuse.ReleaseRequest) error {
-	fmt.Printf("%#v\n", req)
+	cfh.cmd.Process.Signal(syscall.SIGTERM)
 	return nil
 }
 
@@ -138,8 +151,8 @@ var _ fs.FS = (*ShellFileSystem)(nil)
 var _ fs.Node = (*Dir)(nil)
 var _ fs.HandleReadDirAller = (*Dir)(nil)
 var _ fs.NodeStringLookuper = (*Dir)(nil)
-var _ fs.Node = (*File)(nil)
-var _ fs.NodeOpener = (*File)(nil)
+var _ fs.Node = (*CommandFile)(nil)
+var _ fs.NodeOpener = (*CommandFile)(nil)
 var _ fs.Handle = (*CommandFileHandle)(nil)
 var _ fs.HandleReleaser = (*CommandFileHandle)(nil)
 var _ fs.HandleReader = (*CommandFileHandle)(nil)
